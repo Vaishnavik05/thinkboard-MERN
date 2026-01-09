b93c0f4de933cdfd0921fc5db4edb9db93f16e4e
vaishu <vaishu@gmail.com>
2026-01-09 10:22:57 +0530

    Updated API baseURL to Render backend

--- frontend/thinkboard/src/lib/axios.js
+++ frontend/thinkboard/src/lib/axios.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import axios from 'axios'
+
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api', });
+  baseURL: 'https://collaborative-canvas1.onrender.com/api',
+});
+
 export default api;
